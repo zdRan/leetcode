76383686a9cbf423a41df4c2cb02d7873a1cef21
zdRan <cm.zdran@gmail.com>
2017-11-15 13:26:21 +0800

    CHANGE:格式化代码

--- src/com/leetcode/submissions/MedianTwoArrays.java
+++ src/com/leetcode/submissions/MedianTwoArrays.java
@@ -19,13 +19,13 @@ public class MedianTwoArrays {
 
         int num1;
         int num2;
-        while (i3 <= T/2){
-            num1 = (i1 == nums1.length) ? Integer.MAX_VALUE:nums1[i1];
-            num2 = (i2 == nums2.length) ? Integer.MAX_VALUE:nums2[i2];
-            if (num1<num2){
+        while (i3 <= T / 2) {
+            num1 = (i1 == nums1.length) ? Integer.MAX_VALUE : nums1[i1];
+            num2 = (i2 == nums2.length) ? Integer.MAX_VALUE : nums2[i2];
+            if (num1 < num2) {
                 nums3.add(num1);
                 i1++;
-            }else {
+            } else {
                 nums3.add(num2);
                 i2++;
             }
@@ -33,12 +33,12 @@ public class MedianTwoArrays {
         }
 
         int temp;
-        if (T%2 != 0){
-            temp = nums3.get(i3-1);
-            result = (temp + temp)/2.0;
-        }else {
-            temp = nums3.get(i3-2)+nums3.get(i3-1);
-            result = temp/2.0;
+        if (T % 2 != 0) {
+            temp = nums3.get(i3 - 1);
+            result = (temp + temp) / 2.0;
+        } else {
+            temp = nums3.get(i3 - 2) + nums3.get(i3 - 1);
+            result = temp / 2.0;
         }
         return result;
     }
