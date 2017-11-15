e411e01962ca6d2ed88a54cd02a67e47472ed95b
zdRan <cm.zdran@gmail.com>
2017-11-15 16:12:29 +0800

    ADD:添加5. Longest Palindromic Substring的解答

---
+++ src/com/leetcode/submissions/LongestPalindromicSubstring.java
@@ -0,0 +1,14 @@
+package com.leetcode.submissions;
+
+/**
+ * Create by ranzd on 2017/11/15
+ *
+ * @author ranzd@chinaunicom.cn
+ */
+public class LongestPalindromicSubstring {
+
+    public String longestPalindrome(String s) {
+        int[] rad = new int[s.length()];
+        return null;
+    }
+}
