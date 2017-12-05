ee81051787ce25eb0f4cebbfa0cac5d5e8172eef
zdRan <cm.zdran@gmail.com>
2017-12-05 16:49:51 +0800

    CHANGE:临时Test

--- src/com/leetcode/test/TestMain.java
+++ src/com/leetcode/test/TestMain.java
@@ -2,6 +2,11 @@ package com.leetcode.test;
 
 import com.leetcode.utils.Show;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * 用于测试一些API
  * <p>
@@ -16,8 +21,49 @@ public class TestMain {
         Show.showString("index = "+"qqqddd".startsWith("qqq"));
 
         Show.showString("index = "+"qqqddde".substring("qqq".length()));
-        char ch = 65;
+        char ch = '3';
+        ch = (char) (ch -'1');
         System.out.println(ch);
+
+        Show.showString("monotoneIncreasingDigits = "+monotoneIncreasingDigits(332));
+    }
+
+    public static int monotoneIncreasingDigits(int N) {
+
+        String strNum = String.valueOf(N);
+        StringBuilder sb = new StringBuilder();
+        if (strNum.length() == 1){
+            return N;
+        }
+        int index = 1;
+        sb.append(strNum.charAt(0));
+        for (; index < strNum.length(); index++) {
+            if (strNum.charAt(index)>=strNum.charAt(index-1)){
+                sb.append(strNum.charAt(index-1));
+            }else {
+                sb.append("9");
+                int temp = index;
+                temp--;
+                char c = sb.charAt(temp);
+                int num = Integer.parseInt(String.valueOf(c));
+                sb.setCharAt(temp,((num-1)+"").charAt(0));
+                while (temp>0){
+
+                    if (sb.charAt(temp)<sb.charAt(temp-1)){
+                        sb.setCharAt(temp, '9');
+                        char c1 = sb.charAt(temp);
+                        int num1 = Integer.parseInt(String.valueOf(c1));
+                        sb.setCharAt(temp,((num1-1)+"").charAt(0));
+                    }
+                    temp--;
+                }
+                break;
+            }
+        }
+        for (; index < strNum.length()-1; index++) {
+            sb.append("9");
+        }
+        return Integer.parseInt(sb.toString());
     }
 
 }
