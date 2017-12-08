456dc8a3c108f69ce02cbfca05f7122629dc9ffa
zdRan <cm.zdran@gmail.com>
2017-12-08 22:27:53 +0800

    ADD:添加 23. Merge k Sorted Lists

---
+++ src/com/leetcode/mains/MergekListsMain.java
@@ -0,0 +1,48 @@
+package com.leetcode.mains;
+
+import com.leetcode.extend.ListNode;
+import com.leetcode.submissions.MergeTwoLists;
+import com.leetcode.submissions.MergekLists;
+import com.leetcode.utils.Show;
+
+/**
+ * Create by ranzd on 2017/12/7
+ *
+ * @author ranzd@chinaunicom.cn
+ */
+public class MergekListsMain {
+    public static void main(String[] args){
+        MergekLists mergekLists = new MergekLists();
+        ListNode[] lists = new ListNode[3];
+        ListNode l1 = new ListNode(0);
+        ListNode curr = l1;
+        for (int i = 1;i<10;i+=2){
+            curr.next = new ListNode(i);
+
+            curr = curr.next;
+        }
+        lists[0] = l1;
+        ListNode l2 = new ListNode(2);
+        curr = l2;
+        for (int i = 4;i<16;i+=2){
+            curr.next = new ListNode(i);
+
+            curr = curr.next;
+        }
+        lists[1] = l2;
+        ListNode l3 = new ListNode(2);
+        curr = l3;
+        for (int i = 20;i<36;i+=2){
+            curr.next = new ListNode(i);
+
+            curr = curr.next;
+        }
+        lists[2] = l3;
+
+        ListNode head = mergekLists.mergeKLists(lists);
+        while (head!=null){
+            Show.showString("->"+head.val);
+            head = head.next;
+        }
+    }
+}
---
+++ src/com/leetcode/problems/MergekLists.md
@@ -0,0 +1,14 @@
+# 23. Merge k Sorted Lists
+## Description
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+## Example
+```$xslt
+```
+## Translation
+合并k个已排序的链接列表并将其作为一个排序列表返回。分析并描述其复杂性。
+## Ideas
+分治 + 归并排序
+
+将数组进行分治，将分治的结果进行归并排序。 
+## Note
