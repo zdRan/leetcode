b9b13fb6fc82850bfa08be63ed257777be60c446
zdRan <cm.zdran@gmail.com>
2017-12-26 13:09:05 +0800

    ADD:添加 29. Divide Two Integers

---
+++ src/com/leetcode/mains/DivideIntegersMain.java
@@ -0,0 +1,16 @@
+package com.leetcode.mains;
+
+import com.leetcode.submissions.DivideIntegers;
+import com.leetcode.utils.Show;
+
+/**
+ * Create by ranzd on 2017/12/26
+ *
+ * @author ranzd@chinaunicom.cn
+ */
+public class DivideIntegersMain {
+    public static void main(String[] args){
+        DivideIntegers divideIntegers = new DivideIntegers();
+        Show.showString(divideIntegers.divide(-2147483648, 1)+"");
+    }
+}
---
+++ src/com/leetcode/submissions/DivideIntegers.java
@@ -0,0 +1,32 @@
+package com.leetcode.submissions;
+
+/**
+ * Create by ranzd on 2017/12/26
+ *
+ * @author ranzd@chinaunicom.cn
+ */
+public class DivideIntegers {
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0 || (dividend == Integer.MIN_VALUE && (divisor == -1))){
+            return Integer.MAX_VALUE;
+        }
+        //对被除数和除数取绝对值,转换成成 long，防止溢出
+        long dividendAbs = Math.abs((long) dividend);
+        long divisorAbs = Math.abs((long) divisor);
+
+        int quo = 0;
+
+        for (int i = 31;i>=0;i--){
+            //将被除数右移，防止除数左移发生溢出
+            if ((dividendAbs >>i)>= divisorAbs){
+                dividendAbs -= (divisorAbs<<i);
+                quo += (1<<i);
+            }
+        }
+        //确定符号
+        if ((dividend ^ divisor) < 0) {
+            quo = -quo;
+        }
+        return quo;
+    }
+}
