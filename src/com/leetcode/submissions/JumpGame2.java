fd50aab6fc2621710c2fce79e0ea4a5b23b1f79f
zdRan <cm.zdran@gmail.com>
2018-11-18 19:40:23 +0800

    ADD:添加55题解答

---
+++ src/com/leetcode/submissions/JumpGame2.java
@@ -0,0 +1,27 @@
+package com.leetcode.submissions;
+
+/**
+ * Create by ranzd on 2018/11/18
+ *
+ * @author cm.zdran@gmail.com
+ */
+public class JumpGame2 {
+    public static boolean canJump(int[] nums) {
+
+        int max = nums[0];
+        for (int i = 0; i <= max; i++) {
+            max = i + nums[i] > max ? (i + nums[i]) : max;
+            if (max >= nums.length-1) {
+                break;
+            }
+        }
+        return max >= nums.length-1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canJump(new int[]{2, 3, 1, 1, 4}));
+
+        System.out.println(canJump(new int[]{1,2,3}));
+    }
+
+}
