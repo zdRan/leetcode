4064d452f68407ff1dbb514dd0a644fe4cb8e680
zdRan <cm.zdran@gmail.com>
2018-11-19 16:30:12 +0800

    ADD:添加辅助类（区间对象）

---
+++ src/com/leetcode/extend/Interval.java
@@ -0,0 +1,21 @@
+package com.leetcode.extend;
+
+/**
+ * Create by ranzd on 2018/11/19
+ *
+ * @author cm.zdran@gmail.com
+ */
+public class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
