2ba1c62cf615bc9aaed938c07629eb5233c7a65d
zdRan <cm.zdran@gmail.com>
2018-12-11 10:34:06 +0800

    ADD:添加66题解答

---
+++ src/com/leetcode/problems/PlusOne.md
@@ -0,0 +1,34 @@
+# 66. Plus One
+## Description
+Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+## Example
+```
+Example 1:
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+
+Example 2:
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+```
+
+## Translation
+给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+
+最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+
+你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+## Ideas
+遍历数组，加1后超过10进位。
+
+数组遍历结束后，如果还有进位，则创建新数组，保存进位，并复制原来的数组。
+
+## Note
---
+++ src/com/leetcode/submissions/PlusOne.java
@@ -0,0 +1,31 @@
+package com.leetcode.submissions;
+
+import java.util.Arrays;
+
+/**
+ * Create by ranzd on 2018/12/10
+ *
+ * @author ranzd@chinaunicom.cn
+ */
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        int add = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] + add == 10) {
+                digits[i] = 0;
+            } else {
+                digits[i] += add;
+                return digits;
+            }
+        }
+        int[] ansArr = new int[digits.length + 1];
+        ansArr[0] = 1;
+        System.arraycopy(digits, 0, ansArr, 1, ansArr.length - 1);
+        return ansArr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(plusOne(new int[]{9, 9, 9})));
+    }
+
+}
