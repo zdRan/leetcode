ca27401d45a1e7c14105824da30b79c45f4f189c
zdRan <cm.zdran@gmail.com>
2019-04-25 19:09:12 +0800

    ADD:添加91题解答

--- src/com/leetcode/submissions/DecodeWays.java
+++ src/com/leetcode/submissions/DecodeWays.java
@@ -1,27 +1,36 @@
 package com.leetcode.submissions;
-
+/**
+ * 91. 解码方法
+ * Create by ranzd on 2018/2/1
+ *
+ * @author cm.zdran@gmail.com
+ */
 public class DecodeWays {
+    /**
+     * 动态规划：
+     * dp[i] = n 表示 在[0,i+1] 个字符的时候有 n 种可能，
+     *
+     * 比如： dp[3] = 2, 使用前 3 个字符，有 2 种可能
+     *
+     * 动态方程：
+     * 如果 当前位置不为 '0',说明可以独立存在，dp[i] = dp[i-1]
+     * 如果 当前位置与前一个字符组成的数字 在 [10,26]之间，说明可以组合存在，dp[i] = dp[i] + dp[i-2]
+     *
+     * @param s
+     * @return
+     */
     public static int numDecodings(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        if (s.length() == 1) {
-            return s.charAt(0) == '0' ? 0 : 1;
+        if (s.charAt(0) == '0') {
+            return 0;
         }
-        int start = 0;
-        while (start < s.length() && s.charAt(start) == '0') {
-            start++;
-        }
-        if (start == s.length() - 1) {
-            return s.charAt(start) == '0' ? 0 : 1;
-        }
-
         int[] dp = new int[s.length() + 1];
-        dp[start] = 1;
-        dp[start + 1] = 1;
-
-        for (int i = start + 2; i <= s.length(); i++) {
-            if (s.charAt(i - 1) != '0') {
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= s.length(); i++) {
+            if (s.charAt(i - 1) != '0' ) {
                 dp[i] = dp[i - 1];
             }
             int temp = Integer.valueOf(s.substring(i - 2, i));
@@ -31,27 +40,7 @@ public class DecodeWays {
         }
         return dp[s.length()];
     }
-    public static int numDecodings2(String s) {
-        int n = s.length();
-        if (n <= 0) return 0;
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = (s.charAt(0) == '0') ? 0 : 1;
-
-        for (int i = 2; i <= n; i++) {
-            int first = Integer.valueOf(s.substring(i-1,i));
-            int second = Integer.valueOf(s.substring(i-2,i));
-
-            if (first >= 1 && first <= 9)
-                dp[i] += dp[i-1];
-            if (second >= 10 && second <= 26)
-                dp[i] += dp[i-2];
-        }
-
-        return dp[n];
-    }
-
     public static void main(String[] args) {
-        System.out.println(numDecodings2("110"));
+        System.out.println(numDecodings("1100"));
     }
 }
