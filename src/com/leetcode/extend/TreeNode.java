5e40d992421d15d3fda848d0b40665ce4fb3290e
zdRan <cm.zdran@gmail.com>
2019-05-15 16:32:17 +0800

    ADD:添加二叉树节点类

---
+++ src/com/leetcode/extend/TreeNode.java
@@ -0,0 +1,16 @@
+package com.leetcode.extend;
+
+/**
+ * Create by ranzd on 2019/5/15
+ *
+ * @author cm.zdran@gmail.com
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
