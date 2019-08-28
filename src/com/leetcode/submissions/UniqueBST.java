8d10264e84c7a76bdd5e646622390c52b7e40f67
zdRan <cm.zdran@gmail.com>
2019-08-28 21:47:49 +0800

    ADD: 添加 96 题解答

--- src/com/leetcode/submissions/UniqueBST.java
+++ src/com/leetcode/submissions/UniqueBST.java
@@ -26,4 +26,12 @@ public class UniqueBST {
         return G[n];
 
     }
+
+    public int numTrees2(int n){
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
+    }
 }
