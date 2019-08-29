b02df96ec1f6d687e07f5a2ee01dc16a974e4875
zdRan <cm.zdran@gmail.com>
2019-08-29 14:58:10 +0800

    CHANGE: 添加解析

--- src/com/leetcode/submissions/UniqueBST.java
+++ src/com/leetcode/submissions/UniqueBST.java
@@ -1,11 +1,34 @@
 package com.leetcode.submissions;
 
 /**
+ * 96. 不同的二叉搜索树
  * Create by ranzd on 2019/8/27
  *
  * @author cm.zdran@gmail.com
  */
 public class UniqueBST {
+    /**
+     * 动态规划。我们可以定义两个函数：
+     *
+     * G(n): 长度为n的序列的不同二叉搜索树个数。
+     *
+     * F(i, n): 以i为根的不同二叉搜索树个数( 1 <= i <= n)。
+     *
+     * 以 [0,M] 为区间时，G(M) 的结果是以每个 i 为根节点能够组成的二叉树的和，
+     * 所以可以得出：
+     *
+     * G(M) = sum（ F(i,M) ） i<=1 <= M
+     *
+     * 在区间 [0,M] 中 以 k 为根节点时，能够组成的所有二叉树为，
+     * 左子树的结果集与右子树的结果集的组合数，或者是笛卡尔积。
+     * 所以可以得出：
+     *
+     * F(k,M) = sum ( G(k-1) * G(M-k) )
+     *
+     *
+     * @param n
+     * @return
+     */
     public int numTrees(int n) {
         if (n == 0) {
             return 0;
@@ -27,7 +50,7 @@ public class UniqueBST {
 
     }
 
-    public int numTrees2(int n){
+    public int numTrees2(int n) {
         long C = 1;
         for (int i = 0; i < n; ++i) {
             C = C * 2 * (2 * i + 1) / (i + 2);
