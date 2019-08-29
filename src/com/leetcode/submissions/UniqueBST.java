f5b4dd95afe4cea405d5f795ce79070c23ea3fc6
zdRan <cm.zdran@gmail.com>
2019-08-29 11:06:00 +0800

    CHANGE: 修复下标越界的问题

--- src/com/leetcode/submissions/UniqueBST.java
+++ src/com/leetcode/submissions/UniqueBST.java
@@ -19,7 +19,7 @@ public class UniqueBST {
         G[1] = 1;
 
         for (int i = 2; i <= n; i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 1; j < i; j++) {
                 G[i] += G[j - 1] * G[i - j];
             }
         }
