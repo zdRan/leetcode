7bed986343e9779c6c862f4356b6089559cd9542
zdRan <cm.zdran@gmail.com>
2019-09-04 20:42:43 +0800

    ADD: 添加99题解答

---
+++ src/com/leetcode/submissions/RecoverBST.java
@@ -0,0 +1,51 @@
+package com.leetcode.submissions;
+
+import com.leetcode.extend.TreeNode;
+import com.leetcode.utils.TreeUtil;
+
+/**
+ * Create by ranzd on 2019-09-04 11:26
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class RecoverBST {
+    private TreeNode left, right;
+    private TreeNode min = new TreeNode(Integer.MIN_VALUE);
+
+    public void recoverTree(TreeNode root) {
+        dsf(root);
+        int temp = left.val;
+        left.val = right.val;
+        right.val = temp;
+
+    }
+
+    /**
+     *
+     * @param root
+     */
+    private void dsf(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dsf(root.left);
+
+        if (root.val < min.val ) {
+            if (left == null) {
+                left = min;
+            }
+            //相邻两个元素交换了位置，那么 right 应该为 当前的 root
+            right = root;
+        }else {
+            min = root;
+        }
+        dsf(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = TreeUtil.stringToTreeNode("[3,1,4,null,null,2]");
+        RecoverBST recoverBST = new RecoverBST();
+        recoverBST.recoverTree(root);
+
+    }
+}
---
+++ src/com/leetcode/utils/TreeUtil.java
@@ -0,0 +1,79 @@
+package com.leetcode.utils;
+
+import com.leetcode.extend.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Create by ranzd on 2019-09-04 20:18
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class TreeUtil {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+    public static String treeNodeToString(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+
+        String output = "";
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (node == null) {
+                output += "null, ";
+                continue;
+            }
+
+            output += String.valueOf(node.val) + ", ";
+            nodeQueue.add(node.left);
+            nodeQueue.add(node.right);
+        }
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
+}
