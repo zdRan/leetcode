33c3dc4f2e2f2e7834d331eabfc9c607bde0a6a4
zdRan <cm.zdran@gmail.com>
2019-09-06 14:20:16 +0800

    ADD: 添加101题解答

---
+++ src/com/leetcode/submissions/SymmetricTree.java
@@ -0,0 +1,38 @@
+package com.leetcode.submissions;
+
+import com.leetcode.extend.TreeNode;
+import com.leetcode.utils.TreeUtil;
+
+/**
+ * Create by ranzd on 2019-09-06 10:17
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        //同时递归 root 节点的 两个子树
+        return dsf(root.left, root.right);
+    }
+
+    /**
+     * 同时递归 root 节点的 两个子树,当左子树向右遍历时，右子树向左遍历
+     * @param left
+     * @param right
+     * @return
+     */
+    private boolean dsf(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return dsf(left.left, right.right) && dsf(left.right, right.left);
+    }
+}
