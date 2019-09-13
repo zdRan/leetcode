81dbe28fd9135368ece2954f67771fe60f93ec4e
zdRan <cm.zdran@gmail.com>
2019-09-13 16:06:09 +0800

    ADD: 添加102题解答

---
+++ src/com/leetcode/submissions/BTLevelOrderTraversal.java
@@ -0,0 +1,44 @@
+package com.leetcode.submissions;
+
+import com.leetcode.extend.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create by ranzhendong on 2019-09-11
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class BTLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        dsf(result, root, 0);
+        return result;
+    }
+
+    /**
+     * 使用 level 记录树的层数，把相同层的节点添加到 result 中
+     * @param result
+     * @param root
+     * @param level
+     */
+    private void dsf(List<List<Integer>> result,TreeNode root,int level){
+        if (root == null) {
+            return;
+        }
+        if (level >= result.size()) {
+            List<Integer> item = new ArrayList<>();
+            item.add(root.val);
+            result.add(item);
+        }else {
+            result.get(level).add(root.val);
+        }
+        dsf(result, root.left, level+1);
+        dsf(result, root.right, level + 1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
