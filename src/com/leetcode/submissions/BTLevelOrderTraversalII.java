d4f3abfdced1e5ae7dc516f7d502ea4175bffb2b
zdRan <cm.zdran@gmail.com>
2019-09-29 18:49:09 +0800

    ADD: 添加107题解答

---
+++ src/com/leetcode/submissions/BTLevelOrderTraversalII.java
@@ -0,0 +1,38 @@
+package com.leetcode.submissions;
+
+import com.leetcode.extend.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Create by ranzhendong on 2019-09-26 18:01
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class BTLevelOrderTraversalII {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        dsf(root, 0, result);
+
+        for (int left = 0, right = result.size() - 1; left < right; left++, right--) {
+            List<Integer> temp = result.get(left);
+            result.set(left, result.get(right));
+            result.set(right, temp);
+        }
+        return result;
+    }
+
+    private void dsf(TreeNode root, int level, List<List<Integer>> result) {
+        if (root == null) {
+            return;
+        }
+        if (level >= result.size()) {
+            result.add(new LinkedList<>());
+        }
+        result.get(level).add(root.val);
+        dsf(root.left, level + 1, result);
+        dsf(root.right, level + 1, result);
+    }
+}
