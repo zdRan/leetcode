ad0ce06a9db1d9d081ff82ff7febaff031173bbe
zdRan <cm.zdran@gmail.com>
2019-09-30 10:28:48 +0800

    ADD: 添加110题解答

---
+++ src/com/leetcode/submissions/BalancedBT.java
@@ -0,0 +1,36 @@
+package com.leetcode.submissions;
+
+import com.leetcode.extend.TreeNode;
+
+/**
+ * Create by ranzhendong on 2019-09-29 19:20
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class BalancedBT {
+    public boolean isBalanced(TreeNode root) {
+        return dsf(root) != -1;
+    }
+    private int dsf(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+        int left = dsf(root.left);
+        //说明左子树不是平衡二叉树
+        if (left == -1) {
+            return left;
+        }
+        int right = dsf(root.right);
+        //说明右子树不是平衡二叉树
+        if (right == -1) {
+            return right;
+        }
+        // 说明当前节点不是平衡二叉树
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+        //返回当前节点的树的高度
+        return Math.max(left + 1, right + 1);
+    }
+
+}
