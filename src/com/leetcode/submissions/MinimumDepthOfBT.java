7c32a7ca10ed9bdc6d3423d43f513cc3ff7351c0
zdRan <cm.zdran@gmail.com>
2019-09-30 11:16:09 +0800

    ADD: 添加111题解答

---
+++ src/com/leetcode/submissions/MinimumDepthOfBT.java
@@ -0,0 +1,29 @@
+package com.leetcode.submissions;
+
+import com.leetcode.extend.TreeNode;
+import com.leetcode.utils.TreeUtil;
+
+/**
+ * Create by ranzhendong on 2019-09-30 10:30
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class MinimumDepthOfBT {
+    public static int minDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        //如果左右子树有一个为空，树的最小深度则为不是空的子树的最大高度
+        return (left == 0 || right == 0) ? Math.max(left + 1, right + 1) : Math.min(left + 1, right + 1);
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = TreeUtil.stringToTreeNode("[1,2]");
+
+        System.out.println(minDepth(treeNode));
+
+    }
+}
