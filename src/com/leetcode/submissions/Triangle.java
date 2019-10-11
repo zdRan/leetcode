19b97d187e2e3da674709f94f321b31ad6ffe600
zdRan <cm.zdran@gmail.com>
2019-10-11 18:06:43 +0800

    ADD: 添加120题解答

---
+++ src/com/leetcode/submissions/Triangle.java
@@ -0,0 +1,26 @@
+package com.leetcode.submissions;
+
+import java.util.List;
+
+/**
+ * Create by ranzhendong on 2019-10-11 17:28
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class Triangle {
+
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        Integer[] result = triangle.get(triangle.size() - 1).toArray(new Integer[]{});
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                result[j] = Math.min(result[j], result[j + 1]) + triangle.get(i).get(j);
+
+            }
+        }
+        return result[0];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
