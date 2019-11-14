d634ab6f87b1ecf0383b259b4d9ed03bb4aeed14
zdRan <cm.zdran@gmail.com>
2019-11-14 16:34:50 +0800

    ADD: 添加129题解答

---
+++ src/com/leetcode/submissions/SumRootToLeafNumbers.java
@@ -0,0 +1,30 @@
+package com.leetcode.submissions;
+
+import com.leetcode.extend.TreeNode;
+
+/**
+ * Create by Ranzd on 2019-11-13 10:25
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class SumRootToLeafNumbers {
+    public int sumNumbers(TreeNode root) {
+        TreeNode ans = new TreeNode(0);
+        dfs(root, "", ans);
+        return ans.val;
+    }
+
+    private void dfs(TreeNode root, String val, TreeNode ans) {
+        val = val + root.val;
+        if (root.left == null && root.right == null) {
+            ans.val = ans.val + Integer.parseInt(val);
+            return;
+        }
+        if (root.left != null) {
+            dfs(root.left, val, ans);
+        }
+        if (root.right != null) {
+            dfs(root.right, val, ans);
+        }
+    }
+}
