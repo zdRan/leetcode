bb0abdeaa460e5a7ae16ed2cd4e6f3ae4737f146
zdRan <cm.zdran@gmail.com>
2021-02-22 17:25:44 +0800

    448. 找到所有数组中消失的数字

---
+++ src/com/leetcode/submissions/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,41 @@
+package com.leetcode.submissions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 448. 找到所有数组中消失的数字
+ * Create by Ranzd on 2021-02-13 18:15
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class FindAllNumbersDisappearedInAnArray {
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == i + 1) {
+                continue;
+            }
+            int nextIndex = nums[i] - 1;
+            while (true) {
+                if (nums[nextIndex] == nextIndex + 1) {
+                    break;
+                }
+                int temp = nums[nextIndex];
+                nums[nextIndex] = nextIndex+1;
+                nextIndex = temp - 1;
+            }
+        }
+        //System.out.println(Arrays.toString(nums));
+        List<Integer> ans = new ArrayList<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i+1) {
+                ans.add(i+1);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findDisappearedNumbers(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
+    }
+}
---
+++ src/com/leetcode/submissions/MinimumTimeToCollectAllApplesInATree.java
@@ -0,0 +1,55 @@
+package com.leetcode.submissions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 1443. 收集树上所有苹果的最少时间（尝试中）
+ * Create by Ranzd on 2021-02-13 18:59
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class MinimumTimeToCollectAllApplesInATree {
+
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        List<List<Integer>> edgesList = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            edgesList.add( new ArrayList<>(2));
+        }
+        for (int i = 0; i < edges.length; i++) {
+            edgesList.get(edges[i][0]).add(edges[i][1]);
+        }
+        System.out.println(edgesList);
+        int result = 0;
+        for (int i = 0; i < edgesList.get(0).size(); i++) {
+            result+= func(edgesList.get(0).get(i), edgesList, hasApple);
+        }
+        return result * 2;
+    }
+
+    public int func(int root, List<List<Integer>> edgesList , List<Boolean> hasApple) {
+        if (root >= edgesList.size()) {
+            return hasApple.get(root) ? 1 : 0;
+        }
+        int result = 0;
+        for (int i = 0; i < edgesList.get(root).size(); i++) {
+            result+= func(edgesList.get(root).get(i), edgesList, hasApple);
+        }
+        return result > 0 ? result + 1 : hasApple.get(root) ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        MinimumTimeToCollectAllApplesInATree tree = new MinimumTimeToCollectAllApplesInATree();
+        int ans = tree.minTime(7,
+                new int[][]{{0,1},{0,2},{1,4},{1,5},{2,3},{2,6}},
+                Arrays.asList(false,false,true,false,true,true,false));
+        System.out.println(ans);
+
+
+
+    }
+
+
+}
