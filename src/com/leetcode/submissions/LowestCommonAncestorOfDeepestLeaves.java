04ab3823ce13b9dbe80a7a17e248cefbecf61342
zdRan <cm.zdran@gmail.com>
2021-02-25 15:37:46 +0800

    1123. 最深叶节点的最近公共祖先

---
+++ src/com/leetcode/submissions/LowestCommonAncestorOfDeepestLeaves.java
@@ -0,0 +1,31 @@
+package com.leetcode.submissions;
+
+import com.leetcode.extend.TreeNode;
+
+/**
+ * 1123. 最深叶节点的最近公共祖先
+ * Create by Ranzd on 2021-02-23 18:37
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class LowestCommonAncestorOfDeepestLeaves {
+    public TreeNode ans = null;
+    public int max = -1;
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        ans = root;
+        dfs(root, 1);
+        return ans;
+    }
+    public int dfs(TreeNode root,int deep){
+        if (root == null) {
+            return deep - 1;
+        }
+        int left = dfs(root.left, deep + 1);
+        int right = dfs(root.right, deep + 1);
+        if (left == right && max <= left) {
+            ans = root;
+            max = Math.max(max, deep);
+        }
+        return Math.max(left, right);
+    }
+}
