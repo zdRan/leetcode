20d59e677930f6248eb4f0a5cb45760fa354012f
zdRan <cm.zdran@gmail.com>
2021-02-26 14:18:14 +0800

    199. 二叉树的右视图

---
+++ src/com/leetcode/submissions/BinaryTreeRightSideView.java
@@ -0,0 +1,32 @@
+package com.leetcode.submissions;
+
+import com.leetcode.extend.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 199. 二叉树的右视图
+ * Create by Ranzd on 2021-02-26 12:04
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class BinaryTreeRightSideView {
+    List<Integer> result = new LinkedList<>();
+    int maxDeep = -1;
+    public List<Integer> rightSideView(TreeNode root) {
+        func(root, 0);
+        return result;
+    }
+    public void func(TreeNode root,int deep){
+        if (root == null) {
+            return;
+        }
+        if (deep>maxDeep){
+            result.add(root.val);
+            maxDeep = deep;
+        }
+        func(root.right, deep + 1);
+        func(root.left, deep + 1);
+    }
+}
