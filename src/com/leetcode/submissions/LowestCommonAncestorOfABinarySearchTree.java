7d1ec37f0b343e2e125bc5d609945bafc233fcfb
zdRan <cm.zdran@gmail.com>
2021-03-06 21:36:36 +0800

    235. 二叉搜索树的最近公共祖先

---
+++ src/com/leetcode/submissions/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,36 @@
+package com.leetcode.submissions;
+
+import com.leetcode.extend.TreeNode;
+
+/**
+ * 235. 二叉搜索树的最近公共祖先
+ * Create by Ranzd on 2021-03-06 21:34
+ *
+ * @author ranzhendong@maoyan.com
+ */
+public class LowestCommonAncestorOfABinarySearchTree {
+    TreeNode result = null;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root,p.val,q.val);
+        return result;
+    }
+
+    public boolean dfs(TreeNode root, int p, int q){
+        if (root ==null){
+            return false;
+        }
+        boolean left = dfs(root.left,p,q);
+        boolean right = dfs(root.right,p,q);
+
+        if(left && right && result==null){
+            result = root;
+            return true;
+        }
+        if((left||right)&&(root.val == p||root.val == q)&&result==null){
+            result = root;
+            return true;
+        }
+        return left||right||(root.val == p||root.val == q);
+    }
+
+}
